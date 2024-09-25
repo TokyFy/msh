00a49ff165f7bccff18711551904a83d5771486b
tokyfy <tokyfydev@gmail.com>
2024-09-25 14:21:46 +0300

    nothing

--- msh.c
+++ msh.c
@@ -5,13 +5,10 @@ int main(int argc , char **argv)
 	(void)(argc);
 	(void)(argv);
 	char *buff;
-	while(42)
-	{
-		buff = readline("> ");
-		add_history(buff);
-		printf("%s\n" , buff);
-		free(buff);
-	}	
+	buff = readline("> ");
+	add_history(buff);
+	printf("%s\n" , buff);
+	free(buff);
 
 	return (1);
 }
