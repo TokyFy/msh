f2d0c38d042b9f0f0701385302f3fdc8976cb8dd
tokyfy <tokyfydev@gmail.com>
2024-10-23 15:57:33 +0300

    refactring

--- src/exec.c
+++ src/exec.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <fcntl.h>
 #include <msh.h>
 #include <stdio.h>
@@ -19,130 +20,67 @@
 #include <time.h>
 #include <unistd.h>
 
-void	exec_t_cmd(t_cmd *cmd, char **env)
+int	ft_open(char *file, int oflags, int iflags)
 {
-	int		out;
-	int		in;
-	t_list	*redirs;
-	t_redir	*redir;
+	int	fd;
 
-	(void)(env);
-	out = -1;
-	in = -1;
-	redirs = cmd->redirs;
-	while (redirs)
+	fd = open(file, oflags, iflags);
+	if (fd == -1)
 	{
-		if (out != -1)
-		{
-			if (close(out) == -1)
-				perror("close");
-		}
-		if (in != -1)
-		{
-			if (close(in) == -1)
-				perror("close");
-		}
-		redir = redirs->content;
-		if (redir->type == REDIR_O)
-		{
-			out = open(redir->string, O_CREAT | O_TRUNC | O_RDWR, 0644);
-			if (out == -1)
-			{
-				perror(redir->string);
-				exit(1);
-			}
-		}
-		else if (redir->type == APPEND)
-		{
-			out = open(redir->string, O_CREAT | O_APPEND | O_RDWR, 0644);
-			if (out == -1)
-			{
-				perror(redir->string);
-				exit(1);
-			}
-		}
-		else if (redir->type == REDIR_I)
-		{
-			in = open(redir->string, O_RDONLY);
-			if (in == -1)
-			{
-				perror(redir->string);
-				exit(1);
-			}
-		}
-		redirs = redirs->next;
+		perror("Open");
+		exit(1);
 	}
-	if (out != -1)
-	{
-		if (dup2(out, STDOUT_FILENO) == -1)
-		{
-			perror("dup2");
-			exit(1);
-		}
-	}
-	if (in != -1)
-	{
-		if (dup2(in, STDIN_FILENO) == -1)
-		{
-			perror("dup2");
-			exit(1);
-		}
-	}
-	if (out != -1 && close(out) == -1)
-		perror("close");
-	if (in != -1 && close(in) == -1)
-		perror("close");
-	execvp(cmd->argv[0], cmd->argv);
-	perror(cmd->argv[0]);
-	exit(127);
+	return (fd);
 }
 
-void perrorexit(const char *error)
+void	perrorexit(const char *error)
 {
 	perror(error);
 	exit(1);
 }
 
-void ft_pipe(int fds[2])
+void	ft_pipe(int fds[2])
 {
-	if(pipe(fds) == -1)
+	if (pipe(fds) == -1)
 	{
 		perror("Pipe");
 		exit(EXIT_FAILURE);
 	}
 }
 
-pid_t ft_fork()
+pid_t	ft_fork(void)
 {
-	pid_t pid = fork();
-	if(pid == -1)
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
 	{
 		perror("Fork");
 		exit(EXIT_FAILURE);
 	}
-	return pid;
+	return (pid);
 }
 
-void ft_dup2(int old , int new)
+void	ft_dup2(int old, int new)
 {
-	if(dup2(old, new) == -1)
+	if (dup2(old, new) == -1)
 	{
 		perror("Dup");
 		exit(EXIT_FAILURE);
 	}
-	return;
+	return ;
 }
 
-void ft_close(int fd)
+void	ft_close(int fd)
 {
-	if(close(fd) == -1)
+	if (close(fd) == -1)
 	{
 		perror("Close");
 		exit(EXIT_FAILURE);
 	}
 }
 
-void ft_waitpid(pid_t pid , int *status , int op)
+void	ft_waitpid(pid_t pid, int *status, int op)
 {
 	if (waitpid(pid, status, op) == -1)
 	{
@@ -151,45 +89,92 @@ void ft_waitpid(pid_t pid , int *status , int op)
 	}
 }
 
+void	setup_redir(int *in, int *out, t_cmd *cmd)
+{
+	t_list	*redirs;
+	t_redir	*redir;
+
+	if (!cmd->redirs)
+		return ;
+	redirs = cmd->redirs;
+	redir = redirs->content;
+	while (redirs)
+	{
+		if (*out != -1 && (redir->type == APPEND || redir->type == REDIR_O))
+			ft_close(*out);
+		if (*in != -1 && (redir->type == REDIR_I || redir->type == HEREDOC))
+			ft_close(*in);
+		if (redir->type == REDIR_O)
+			*out = ft_open(redir->string, O_CREAT | O_TRUNC | O_RDWR, 0644);
+		else if (redir->type == APPEND)
+			*out = ft_open(redir->string, O_CREAT | O_APPEND | O_RDWR, 0644);
+		else if (redir->type == REDIR_I)
+			*in = ft_open(redir->string, O_RDONLY, 0644);
+		redirs = redirs->next;
+		redir = redirs->content;
+	}
+}
+
+void	exec_t_cmd(t_cmd *cmd, char **env)
+{
+	int	out;
+	int	in;
+
+	(void)(env);
+	out = -1;
+	in = -1;
+	setup_redir(&in, &out, cmd);
+	if (out != -1)
+	{
+		ft_dup2(out, STDOUT_FILENO);
+		ft_close(out);
+	}
+	if (in != -1)
+	{
+		ft_dup2(in, STDIN_FILENO);
+		ft_close(in);
+	}
+	execvp(cmd->argv[0], cmd->argv);
+	perror(cmd->argv[0]);
+	exit(127);
+}
+
+void	exec_pipe(void *ast, int *pid1, int *pid2)
+{
+	int	fds[2];
+
+	ft_pipe(fds);
+	*pid1 = ft_fork();
+	if (*pid1 == 0)
+	{
+		ft_close(fds[1]);
+		ft_dup2(fds[0], STDIN_FILENO);
+		ft_close(fds[0]);
+		exec_ast(((t_pipe *)ast)->right);
+	}
+	*pid2 = ft_fork();
+	if (*pid2 == 0)
+	{
+		ft_close(fds[0]);
+		ft_dup2(fds[1], STDOUT_FILENO);
+		ft_close(fds[1]);
+		exec_ast(((t_pipe *)ast)->left);
+	}
+	ft_close(fds[0]);
+	ft_close(fds[1]);
+}
+
 void	exec_ast(void *ast)
 {
-	t_node	*node;
-	t_cmd	*cmd;
-	t_pipe	*pipe_nodes;
-	int		fds[2];
-	int		status;
-	int pid1;
-	int pid2;
-	node = ast;
-	cmd = NULL;
-	if (node->type == CMD)
+	int	status;
+	int	pid1;
+	int	pid2;
+
+	if (((t_node *)ast)->type == CMD)
+		exec_t_cmd((t_cmd *)ast, NULL);
+	if (((t_node *)ast)->type == PIPE)
 	{
-		cmd = ast;
-		exec_t_cmd(cmd, NULL);
-		return ;
-	}
-	if (node->type == PIPE)
-	{
-		pipe_nodes = ast;
-		ft_pipe(fds);
-		pid1 = ft_fork();
-		if (pid1 == 0)
-		{
-			ft_close(fds[1]);
-			ft_dup2(fds[0], STDIN_FILENO);
-			ft_close(fds[0]);
-			exec_ast(pipe_nodes->right);
-		}
-		pid2 = ft_fork();
-		if (pid2 == 0)
-		{
-			ft_close(fds[0]);
-			ft_dup2(fds[1], STDOUT_FILENO);
-			ft_close(fds[1]);
-			exec_ast(pipe_nodes->left);
-		}
-		ft_close(fds[0]);
-		ft_close(fds[1]);
+		exec_pipe(ast, &pid1, &pid2);
 		ft_waitpid(pid1, &status, 0);
 		ft_waitpid(pid2, &status, 0);
 		exit(status);
