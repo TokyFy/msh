dbf62a2b8b05cac4a12aae7caa6d9073772aeb2e
tokyfy <tokyfydev@gmail.com>
2024-10-30 10:04:33 +0300

    Get ctrl+c working

--- src/signals.c
+++ src/signals.c
@@ -16,28 +16,28 @@
 #include <string.h>
 #include <unistd.h>
 
-extern volatile sig_atomic_t g_signal_received;
+extern volatile sig_atomic_t	g_signal_received;
 
-void handle_sigint(int sig)
+void	handle_sigint(int sig)
 {
-    (void)sig;
-    g_signal_received = 1;
-    write(STDOUT_FILENO, "\n", 1);    // Print newline
-    rl_on_new_line();                 // Inform readline about the newline
-    rl_replace_line("", 0);           // Clear the current line
-    rl_redisplay();                   // Redisplay the prompt
+	(void)sig;
+	g_signal_received = 1;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-void setup_signal_handling(void)
+void	setup_signal_handling(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = handle_sigint;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    if (sigaction(SIGINT, &sa, NULL) == -1)
-    {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
+	sa.sa_handler = handle_sigint;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
 }
